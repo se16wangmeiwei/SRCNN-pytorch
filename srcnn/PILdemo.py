0c90e4626f6c7a7259006e8d053064d26ac25538
wangjia <1145743063@qq.com>
2020-10-28 16:38:04 +0800

    srcnn train test and predict code(2)

--- srcnn/PILdemo.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import matplotlib.pyplot as plt
-
-img = Image.open("../test_data/B101/000t1.bmp")
-
-plt.figure("1")
-plt.imshow(img)
-plt.show()
--- srcnn/data.py
+++ srcnn/data.py
@@ -1,35 +1,8 @@
-import tarfile
-from os import remove
-from os.path import exists, join, basename
-
-from six.moves import urllib
 from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize
 from PIL import Image
 
 from dataset import DatasetFromFolder
 
-
-# def download_bsd300(dest="./dataset"):
-#     output_image_dir = join(dest, "BSDS300/images")
-#
-#     if not exists(output_image_dir):
-#         url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
-#         print("downloading url ", url)
-#
-#         data = urllib.request.urlopen(url)
-#
-#         file_path = join(dest, basename(url))
-#         with open(file_path, 'wb') as f:
-#             f.write(data.read())
-#
-#         print("Extracting data")
-#         with tarfile.open(file_path) as tar:
-#             for item in tar:
-#                 tar.extract(item, dest)
-#
-#         remove(file_path)
-#
-#     return output_image_dir
 
 def load_img(filepath):
     img = Image.open(filepath).convert('YCbCr')
